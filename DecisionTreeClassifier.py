ccaf74e8925cc56d13fa9a9720531c5937570763
jiyl <jiyl@coscon.com>
2020-03-11 19:55:53 +0800

    decisionTree

--- DecisionTreeClassifier.py
+++ DecisionTreeClassifier.py
@@ -8,8 +8,9 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
 iris = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3)
+x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3)
 
 clf = DecisionTreeClassifier()
-clf.fit(X_train, y_train)
-print(clf.score(X_test, y_test))
+clf.fit(x_train, y_train)
+score = clf.score(x_test, y_test)
+print(score)

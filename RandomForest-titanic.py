92534d8c0eef0ba1f92a59d35635a93fdd14d9dc
Jyljack <yunleiji9@gmail.com>
2020-03-11 22:38:11 +0800

    sklearn

--- RandomForest-titanic.py
+++ RandomForest-titanic.py
@@ -1,37 +1,43 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2019/2/17 10:29
+# @Time    : 2019/2/17 10:46
 # @Author  : jiyl
-# @File    : DecisionTree-titanic.py
+# @File    : RandomForest-titanic.py
 
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_extraction import DictVectorizer
 
 """
-决策树对泰坦尼克号进行预测生死
+随机森林对泰坦尼克号进行预测生死
 """
 # 获取数据
 
 titan = pd.read_csv("http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/titanic.txt")
 
-# 处理数据，找出特征值和目标值
 x = titan[['pclass', 'age', 'sex']]
 y = titan['survived']
 
-# 缺失值处理
 x1 = x.fillna(x.mean()['age']).copy()
 
-# 分割数据集到训练集合测试集
 x_train, x_test, y_train, y_test = train_test_split(x1, y, test_size=0.25)
 
 # 进行处理（特征工程）特征-》类别-》one_hot编码
 dict = DictVectorizer(sparse=False)
 x_train = dict.fit_transform(x_train.to_dict(orient="records"))
+print(dict.get_feature_names())
 x_test = dict.transform(x_test.to_dict(orient="records"))
 
-dec = DecisionTreeClassifier()
-dec.fit(x_train, y_train)
+rf = RandomForestClassifier(n_jobs=-1)
 
-score = dec.score(x_test, y_test)
+param = {"n_estimators": [120, 200, 300, 500, 800, 1200], "max_depth": [5, 8, 15, 25, 30]}
+
+# 网格搜索与交叉验证
+gc = GridSearchCV(rf, param_grid=param, cv=2)
+
+gc.fit(x_train, y_train)
+
+score = gc.score(x_test, y_test)
+
 print(score)
+print("查看选择的参数模型：", gc.best_params_)
